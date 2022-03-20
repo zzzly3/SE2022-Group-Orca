ac7385b74302a8db83d2f303f6820fb031850e09
blesindis <71581447+blesindis@users.noreply.github.com>
2022-03-20 19:21:20 +0800

    Cors fix

--- back/src/main/java/com/orca/back/config/WebConfig.java
+++ back/src/main/java/com/orca/back/config/WebConfig.java
@@ -1,30 +1,42 @@
 package com.orca.back.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-public class WebConfig implements WebMvcConfigurer {
 
+@Configuration
+public class WebConfig implements WebMvcConfigurer{
 
-    private CorsConfiguration corsConfig(){
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-
-        corsConfiguration.addAllowedOrigin("http://localhost:9876");
-        corsConfiguration.addAllowedHeader("*");
-        corsConfiguration.addAllowedMethod("*");
-        corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setMaxAge(3600L);
-        return corsConfiguration;
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:9876")
+                .allowedHeaders(ALL)
+                .allowedMethods("*");
     }
 
     @Bean
-    public CorsFilter corsFilter(){
+    public FilterRegistrationBean corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", corsConfig());
-        return new CorsFilter(source);
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:9876");
+        config.addAllowedHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS);
+        config.addAllowedMethod(HttpMethod.POST);
+
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
     }
 }
