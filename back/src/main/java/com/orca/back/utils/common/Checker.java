d1478888f2770c2e727c6788aad167a592005d68
blesindis <71581447+blesindis@users.noreply.github.com>
2022-03-20 23:30:24 +0800

    fix idea warnings

--- back/src/main/java/com/orca/back/utils/common/Checker.java
+++ back/src/main/java/com/orca/back/utils/common/Checker.java
@@ -1,20 +1,13 @@
 package com.orca.back.utils.common;
 
 import com.orca.back.entity.User;
-import com.orca.back.mapper.UserMapper;
-import com.orca.back.utils.constants.CommonCode;
 import com.orca.back.utils.constants.ErrorCode;
 
-import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
 public class Checker {
 
-    @Resource
-    UserMapper userMapper;
-
     public ErrorCode checkRegistry(User user){
-        String success = CommonCode.SUCCESS_CODE;
 
         /*身份证号*/
         if (user.getIdentifier() == null)
@@ -35,9 +28,9 @@ public class Checker {
         if (user.getEmail() != null && user.getEmail().length() > 0)
         {
             String email = user.getEmail();
-            Integer apos = email.indexOf('@');
-            Integer dotpos = email.indexOf('.');
-            if (apos < 1 || dotpos - apos < 2)
+            int aPos = email.indexOf('@');
+            int dotPos = email.indexOf('.');
+            if (aPos < 1 || dotPos - aPos < 2)
                 return ErrorCode.E_105;
         }
         /*姓名*/
@@ -45,9 +38,8 @@ public class Checker {
             return ErrorCode.E_106;
         else {
             String name = user.getName();
-            char c[] = name.toCharArray();
-            for(int i = 0; i < c.length; i++)
-                if (!isAlpha(c[i]) && !isHan(c[i]))
+            for(int i = 0; i < name.length(); i++)
+                if (!isAlpha(name.charAt(i)) && !isHan(name.charAt(i)))
                     return ErrorCode.E_106;
         }
         /*学号*/
@@ -78,27 +70,15 @@ public class Checker {
     public ErrorCode checkPassword(String pw){
         if (pw.length() > 32 || pw.length() < 6)
             return ErrorCode.E_108;
-        Integer alpha = 0, digit = 0, special = 0;
+        int alpha = 0, digit = 0, special = 0;
         for (int i = 0; i < pw.length(); i++){
             char c = pw.charAt(i);
             if (isAlpha(c)) alpha = 1;
             if (isDigit(c)) digit = 1;
             if (isSpecial(c)) special = 1;
         }
-        Integer vary = alpha + digit + special;
+        int vary = alpha + digit + special;
         if (vary < 2) return ErrorCode.E_108;
-        return null;
-    }
-
-
-    public ErrorCode checkAdmin(HttpServletRequest request){
-        Integer u_id = (Integer) request.getSession().getAttribute("UserId");
-        if (u_id == null){
-            return ErrorCode.E_109;
-        }
-        User admin = userMapper.selectById(u_id);
-        if (admin.getIsAdmin() == 0)
-            return ErrorCode.E_111;
         return null;
     }
 
@@ -113,26 +93,18 @@ public class Checker {
 
 
     public boolean isAlpha(char c){
-        if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
-            return true;
-        return false;
+        return (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z');
     }
 
     public boolean isHan(char c){
-        if (c >= 19968 && c <= 40869)
-            return true;
-        return false;
+        return (c >= 19968 && c <= 40869);
     }
 
     public boolean isDigit(char c){
-        if (c >= '0' && c <= '9')
-            return true;
-        return false;
+        return (c >= '0' && c <= '9');
     }
 
     public boolean isSpecial(char c){
-        if (c == '-' || c == '_')
-            return true;
-        return false;
+        return  (c == '-' || c == '_');
     }
 }
