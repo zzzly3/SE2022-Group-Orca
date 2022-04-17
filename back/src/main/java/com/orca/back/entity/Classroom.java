0724ec21a75fbdae313027a7e6086e3e7c64a8de
lingnickTang <19307130030@fudan.edu.cn>
2022-04-17 17:36:42 +0800

    lab2 add backend

--- back/src/main/java/com/orca/back/entity/Classroom.java
+++ back/src/main/java/com/orca/back/entity/Classroom.java
@@ -11,6 +11,7 @@ public class Classroom {
     private String name;
     private String building;
     private boolean open;
+    private boolean old;
 
     public String getName(){return name;}
     public void setName(String name){this.name=name;}
@@ -20,4 +21,7 @@ public class Classroom {
 
     public boolean getOpen(){return open;}
     public void setOpen(boolean open){this.open=open;}
+
+    public boolean getOld(){return old;}
+    public void setOld(boolean old){this.old=old;}
 }
